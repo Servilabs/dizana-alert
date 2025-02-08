ad7a6f30c92b84c2d217b812075b26fcc8a016e0
Jorge <thepurplelefant1@gmail.com>
2025-02-08 21:25:56 +0100

    Creation of dizana alert plugin

--- src/main/java/com/dizanaalert/DizanaAlertConfig.java
+++ src/main/java/com/dizanaalert/DizanaAlertConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.DizanaAlert;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface DizanaAlertConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
---
+++ src/main/java/com/dizanaalert/DizanaAlertOverlay.java
@@ -0,0 +1,4 @@
+package com.dizanaalert;
+
+public class DizanaAlertOverlay {
+}
